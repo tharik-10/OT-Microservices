02d31ba0046585db4eb245d64e9bed5d8a065e6c
tharik-10 <md.tharik@mygurukulam.co>
2026-02-05 15:36:05 +0530

    Update SpringDataSalaryService.java

--- salary/src/main/java/com/opstree/microservice/salary/service/SpringDataSalaryService.java
+++ salary/src/main/java/com/opstree/microservice/salary/service/SpringDataSalaryService.java
@@ -2,21 +2,7 @@ package com.opstree.microservice.salary.service;
 
 import com.opstree.microservice.salary.entity.SalaryDef;
 import java.util.List;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.stereotype.Service;
 
-@Service
-public class ElasticsearchRestTemplateServiceImpl implements SpringDataSalaryService {
-
-    private final ElasticsearchRestTemplate elasticsearchRestTemplate;
-
-    public ElasticsearchRestTemplateServiceImpl(ElasticsearchRestTemplate elasticsearchRestTemplate) {
-        this.elasticsearchRestTemplate = elasticsearchRestTemplate;
-    }
-
-    @Override
-    public List<SalaryDef> getSalary() {
-        // Logic for Elasticsearch template
-        return null; 
-    }
+public interface SpringDataSalaryService {
+    List<SalaryDef> getSalary();
 }
